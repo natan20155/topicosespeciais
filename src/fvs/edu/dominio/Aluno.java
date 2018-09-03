5975375b9c007ac428e2fb444a8b277b8706a28e
Natan Silva Ferreira <natansilva20155@gmail.com>
2018-09-03 20:07:39 -0300

    Aula JPA 1

--- src/fvs/edu/dominio/Aluno.java
+++ src/fvs/edu/dominio/Aluno.java
@@ -2,9 +2,17 @@ package fvs.edu.dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
 public class Aluno implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	private String cpf;
